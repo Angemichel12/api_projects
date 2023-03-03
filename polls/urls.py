39429c070921fa8370ea33b75b6b7dca1dadbd50
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-03-03 16:25:46 +0200

    add delete, put option in project

--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-from .views import poll_list, PollDetail,ChoiceList,CreateVote, PollViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('polls', PollViewSet, basename='polls')
-
+from .views import poll_list, poll_detail   
 
 
 urlpatterns = [
     path("polls/", poll_list, name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
-    path("polls/<int:pk>/choices/",ChoiceList.as_view(), name='choices_list'),
-    path('polls/<int:pk>/choices/<int:choice_pk>/vote/', CreateVote.as_view(), name='create_vote'),
+    path("polls/<int:pk>/", poll_detail, name="polls_detail"),
+   
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,47 +1,44 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework import generics, status
-from rest_framework.views import APIView
-from .models import Poll, Choice, Vote
-from .serializers import PollSerializer,ChoiceSerializer,VoteSerializer
-from rest_framework import viewsets
-from rest_framework import status
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from rest_framework.decorators import api_view
+from .models import Poll
+from .serializers import PollSerializer
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 
-class PollViewSet(viewsets.ModelViewSet):
-    polls = Poll.objects.all()
-    serializer_class = PollSerializer
+
+
+
 
 @api_view(['GET', 'POST'])
 def poll_list(request):
     if request.method == 'GET':
         polls = Poll.objects.all()
-        serializer = PollSerializer(polls, many=True)
-        return Response({'polls':serializer.data})
-    if request.method == 'POST':
-        serializer = PollSerializer(data=request.data)
+        serializer = PollSerializer(polls, many=True).data
+        return Response(serializer)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = PollSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-   
-class PollDetail(generics.RetrieveDestroyAPIView):
-    queryset = Poll.objects.all()
-    serializer_class = PollSerializer
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
-class ChoiceList(generics.ListCreateAPIView):
-    def get_queryset(self):
-        queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
-        return queryset
-    serializer_class = ChoiceSerializer
-class CreateVote(APIView):
-    serializer_class = VoteSerializer
-    def post(self, request, pk, choice_pk):
-        voted_by = request.data.get("voted_by")
-        data = {'choice': choice_pk, 'poll':pk, 'voted_by':voted_by}
-        serializer = VoteSerializer(data=data)
+@api_view(['GET', 'PUT', 'DELETE'])
+def poll_detail(request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+    if request.method == 'GET':
+        serializer = PollSerializer(poll).data
+        return Response(serializer)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PollSerializer(poll, data=data)
         if serializer.is_valid():
-            vote = serializer.save()
-            return Response(serializer.data, status=status.HTTP_2O1_CREATED)
-        else:
-            return  Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'DELETE':
+        poll.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
